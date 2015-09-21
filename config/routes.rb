6a85825287194150548a2c5641cbeb8133eea72c
ikukhar <ivkukhar@gmail.com>
2015-09-21 16:44:49 +0300

    Redirecting all routes to root except contains /api/ in url

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   root 'static_pages#index'
-  # get '*path', to: 'static_pages#index'
+  get '*path', to: 'static_pages#index', constraints: lambda { |request| !request.url.include?('/api/') }
 
-  scope '/api' do
+  scope 'api' do
     mount_devise_token_auth_for 'User', at: '/auth'
     resources :projects
   end
